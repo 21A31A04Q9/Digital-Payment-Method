9a1db408598c19557f312ce93f058701bd34c1a3
21A31A04Q9 <146417966+21A31A04Q9@users.noreply.github.com>
2024-10-14 18:48:50 +0530

    Add files via upload

---
+++ User.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self):
+        self.fullName = None 
+        self.mobileNumber = None 
+        self.pin = None 
+        self.defaultBankAccount = None
+        self.previousTransactions = []
+        self.linkedBankAccounts = []
+ 
